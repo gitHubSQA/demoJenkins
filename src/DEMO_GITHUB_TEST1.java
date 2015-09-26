aae1c36f8fe01fa78a34d58a2b3cfb1a9defb021
JDenny <jjdenny@gmail.com>
2015-09-26 00:57:56 -0400

    1st time to Commit

---
+++ src/DEMO_GITHUB_TEST1.java
@@ -0,0 +1,38 @@
+import org.testng.annotations.*;
+
+public class DEMO_GITHUB_TEST1 {
+
+	@BeforeSuite
+	public void unitTest_Start(){
+		System.out.println("---------- Let the test begins ----------");
+	}
+	
+	@BeforeMethod
+	public boolean unitTest_Setup(){
+		return true;
+	}
+	
+	
+	
+	@Test
+	public void unitTest_Test1(){
+		System.out.println("Test1");	
+	}
+	
+	@Test
+	public void unitTest_Test2(){
+		System.out.println("Test2");	
+	}
+	
+	
+	@AfterMethod
+	public boolean unitTest_End(){
+		return false;
+	}
+	
+	@AfterSuite
+	public void unitTest_(){
+		System.out.println("---------- Let the test ends ----------");	
+	}
+	
+}
